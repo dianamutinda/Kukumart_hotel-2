f9158599dc4a68a9f3d1710082fcc4162927ca8d
dianamutinda <jaydiana25@gmail.com>
2025-11-12 17:40:00 +0300

    moving the pages and components to scr

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "../Components/Header"
-import Rooms from "../pages/Rooms";
-import Home from "../pages/Home"
-import About from "../pages/About";
-import Gallery from "../pages/Gallery";
-import Menu from "../pages/Menu";
-import Contact from "../pages/Contact";
-import BookingPage from "../pages/BookingPage";
-import Footer from "../Components/Footer"
+import Header from "./Components/Header"
+import Rooms from "./pages/Rooms";
+import Home from "./pages/Home"
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import BookingPage from "./pages/BookingPage";
+import Footer from "./Components/Footer"
 function App() {
   return (
     <Router>
